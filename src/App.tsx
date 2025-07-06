6b0af6020950a37ed0a1fcec2ba0184a2fe06582
zayn <ltlehro@gmail.com>
2025-07-06 14:18:28 +0500

    created counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import AdminInfo from "./components/AdminInfo";
-import UserInfo from "./components/UserInfo";
-import type { Info, AdminInfoList } from "./types";
+import Counter from "./components/Counter";
 
 const App = () => {
-  const user: Info = {
-    id: 1,
-    name: "zayn",
-    email: "zayn123@gmail.com",
-  };
-
-  const admin: AdminInfoList = {
-    id: 2,
-    name: "ayeza",
-    email: "ayeza123@gmail.com",
-    role: "admin",
-    lastLogin: new Date(),
-  };
-
   return (
     <div>
-      <UserInfo user={user} />
-      <AdminInfo admin={admin} />
+      <Counter />
     </div>
   );
 };
---
+++ src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
